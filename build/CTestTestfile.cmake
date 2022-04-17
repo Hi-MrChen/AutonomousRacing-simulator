dafa7e1df52bbcc5f1b6d2056bfdfaacdb04352a
Hi-MrChen <1845054436@qq.com>
2022-04-17 13:56:04 +0800

    complete

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/alexzheng/Documents/GitHub/AutonomousRacing-simulator/src
+# Build directory: /home/alexzheng/Documents/GitHub/AutonomousRacing-simulator/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("f1tenth_simulator")
---
+++ build/f1tenth_simulator/CMakeFiles/recorder1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/alexzheng/Documents/GitHub/AutonomousRacing-simulator/devel/lib/f1tenth_simulator/recorder1"
+  "/home/alexzheng/Documents/GitHub/AutonomousRacing-simulator/devel/lib/f1tenth_simulator/recorder1.pdb"
+  "CMakeFiles/recorder1.dir/node/recorder1.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/recorder1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
